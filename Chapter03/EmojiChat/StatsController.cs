f711a20e2c018b941c871a24c10ab10038ab2305
Barry Wallis <barry.wallis@outlook.com>
2022-03-27 22:10:00 -0700

    Chapter 3

---
+++ Chapter03/EmojiChat/StatsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace EmojiChat;
+
+[Route("stats/{action}")]
+public class StatsController : ControllerBase
+{
+    private readonly IConfiguration configuration;
+
+    public StatsController(IConfiguration config) => configuration = config;
+
+    [HttpGet]
+    public UserStats Get(int userId)
+    {
+        UserStats userStats = new();
+        string connectionString = configuration.GetConnectionString("DB");
+        using SqlConnection sqlConnection = new(connectionString);
+        sqlConnection.Open();
+        SqlCommand sqlCommand = sqlConnection.CreateCommand();
+        sqlCommand.CommandText = "SELECT COUNT(*) FROM Messages WHERE FromId={0}";
+        _ = sqlCommand.Parameters.Add(userId);
+        userStats.Sent = (int)sqlCommand.ExecuteScalar();
+        sqlCommand.CommandText = "SELECT COUNT(*) FROM Messages WHERE ToId={0}";
+        userStats.Received = (int)sqlCommand.ExecuteScalar();
+        return userStats;
+    }
+}
---
+++ Chapter03/EmojiChat/UserStats.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmojiChat;
+
+/// <summary>
+/// Per user statistics.
+/// </summary>
+public class UserStats
+{
+    /// <summary>
+    /// Number of bytes recieved by the user.
+    /// </summary>
+    public int Received { get; set; }
+
+    /// <summary>
+    /// Number of bytes sent by the user.
+    /// </summary>
+    public int Sent { get; set; }
+}
